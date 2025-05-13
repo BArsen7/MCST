0462ebce1086835892e5437134a33349acda80a3
Arseniy Babanov <arseniy.babanov@yandex.ru>
2025-05-13 21:40:59 +0300

    Расширен перечень информации о файле

--- task-6/script.c
+++ task-6/script.c
@@ -10,8 +10,18 @@ int main(int argc, char *argv[]){
     }
     struct stat *buf;
     if (stat (argv[1], buf)==0){
-        printf("ИНформация о файле:\n");
-        printf("Размер файла: %d байт\n");
+        printf("Информация о файле:\n");
+        printf(" - Режим файла: %d\n", buf->st_mode);
+        printf(" - Номер описателя файла: %d\n", buf->st_ino);
+        printf(" - Идент. устройства, содержащего каталог с входом в этот файл: %d\n", buf->st_dev);
+        //printf("Идент.  устройства", buf->st_rdev); определено только лдля специальных ил  блочных файлов
+        printf(" - Количество ссылок: %d\n", buf->st_nlink);
+        printf(" - Идент. владельца файла: %d\n", buf->st_uid);
+        printf(" - Идент. группы владельца файла: %d\n", buf->st_gid);
+        printf(" - Размер файла: %d байт\n",buf->st_size);
+        //printf(" - Время последнего доступа к файлу: %d\n", buf->st_atime);
+        //printf(" - Время последней записи в файл: %d\n", buf->st_mtime);
+        //printf(" - Время  последнего изменения статуса файла: %d\n", buf->st_ctime);
     }else {
         perror("Ошибка");
     }
