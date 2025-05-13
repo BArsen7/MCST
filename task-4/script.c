5b9a8eb13f9f04225dcb6a2f8b55be4c05a35132
Arseniy Babanov <arseniy.babanov@yandex.ru>
2025-05-13 17:58:22 +0300

    Добавлена обработка длинных аргументов

--- task-4/script.c
+++ task-4/script.c
@@ -7,7 +7,8 @@
 
 
 int main(int argc, char *argv[]) {
-   int rez = 0;
+    /*   
+    int rez = 0;
     opterr = 0;
     while ( (rez = getopt(argc, argv, "mcst")) != -1){
         switch (rez) {
@@ -17,5 +18,27 @@ int main(int argc, char *argv[]) {
             case 't': printf("found argument \"t\"\n"); break;
             case '?': printf("Error found !\n"); break;
         }
-    }   
+    }
+    */
+    int flag_long;
+    int rez;
+ 
+    const char* short_options = "mcst";
+ 
+    const struct option long_options[] = {
+            { "elbrus:", 2, NULL, -2 },
+            { NULL, 0, NULL, 0}
+    };
+ 
+    while ((rez = getopt_long(argc, argv, short_options,
+            long_options, NULL))!=-1){
+            switch (rez) {
+            case 'm': printf("found argument \"m\"\n"); break;
+            case 'c': printf("found argument \"c\"\n"); break;
+            case 's': printf("found argument \"s\"\n"); break;
+            case 't': printf("found argument \"t\"\n"); break;
+            case -2: printf("found argument \"elbrus = %s\"\n", optarg); break;
+            case '?': printf("Error found !\n"); break;
+            }
+    }
 }
